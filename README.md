c1fc45de2e7def9274a342d2bdfe62223dce0320
MikkelMH <mmh@4mvideo.dk>
2022-03-29 18:46:10 +0200

    Initial commit with project and readme files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # iTunesSearcher
+
+A small project to test out the iTunes API which can be found at
+https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/index.html#//apple_ref/doc/uid/TP40017632-CH3-SW1
+
---
+++ iTunesSearcher/iTunesSearcher/iTunesSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearcherApp.swift
+//  iTunesSearcher
+//
+//  Created by 4mvideo it on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
